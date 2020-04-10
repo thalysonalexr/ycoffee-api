c037e836dcb3d5829ef0d5625af2714b593d7643
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-04-10 14:34:58 -0400

    add middleware auth

---
+++ backend/src/app/middlewares/auth.ts
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
+
+import { IMiddleware } from '@core/middlewares/IMiddleware'
+
+export class Auth implements IMiddleware {
+  public process(req: Request, res: Response, next: NextFunction): Response<any> | void {
+    const header = req.headers.authorization
+    
+    if (!header)
+      return res.status(401).json({ error: 'No token provided.' })
+
+    const parts = header.split(' ')
+
+    if (parts.length !== 2)
+      return res.status(401).json({ error: 'Error token.' })
+
+    const [scheme, token] = parts
+
+    if (!/^Bearer$/i.test(scheme))
+      return res.status(401).json({ error: 'Token malformatted.' })
+
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+      if (err)
+        return res.status(401).json({ error: 'Token invalid.' })
+
+      req.sessionId = (decoded as { id: number }).id
+      return next()
+    })
+  }
+}
+
+export default new Auth().process
