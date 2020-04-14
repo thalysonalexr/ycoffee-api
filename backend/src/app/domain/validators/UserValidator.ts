c9686a25eb2623d9f6ecb366ae6a4aa1b3b850a9
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-04-13 23:59:39 -0400

    adding validator to update user

--- backend/src/app/domain/validators/UserValidator.ts
+++ backend/src/app/domain/validators/UserValidator.ts
@@ -29,3 +29,36 @@ export class StoreUser implements IValidator {
     return new StoreUser().run()
   }
 }
+
+export class UpdateUser implements IValidator {
+  public run() {
+    return [
+      header('Content-Type')
+        .equals('application/json')
+        .withMessage('Content-Type: application/json is required.'),
+      
+      header('Authorization')
+        .exists()
+        .withMessage('Set Bearer Token in authorization request.'),
+
+      body('name')
+        .exists()
+        .withMessage('Name is required.')
+        .trim(),
+
+      body('email')
+        .isEmail()
+        .withMessage('Enter a valid e-mail.')
+        .isLength({ max: 100 })
+        .withMessage('Exceeded size for email field.'),
+
+      body('password')
+        .isLength({ min: 5, max: 255 })
+        .withMessage('The password must contain between 5 and 255 characters.'),
+    ]
+  }
+
+  public static instance() {
+    return new UpdateUser().run()
+  }
+}
