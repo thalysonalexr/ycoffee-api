6499b1a7034e5d5cc9a7607c72166b6fcd9a400b
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-04-19 13:24:37 -0400

    create functions helpers

---
+++ backend/src/app/utils/filterObjectFields.spec.ts
@@ -0,0 +1,21 @@
+import faker from 'faker'
+
+import { filterObjectFields } from '@app/utils'
+
+test('should be able filter undefined and exclude properties in object', () => {
+  const obj = {
+    id: undefined,
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    url: faker.internet.url(),
+  }
+
+  const result = filterObjectFields(obj, 'url')
+
+  expect(result).toStrictEqual(
+    expect.objectContaining({
+      name: obj.name,
+      email: obj.email
+    })
+  )
+})
---
+++ backend/src/app/utils/generateObjectFilters.spec.ts
@@ -0,0 +1,23 @@
+import faker from 'faker'
+
+import { generateObjectFilters } from '@app/utils'
+import { IValueObject } from '@core/values/IValueObject'
+
+import { Name, Email } from '@domain/values/User'
+
+test('should be able create object with filters params', () => {
+  const name = faker.name.findName()
+  const email = faker.internet.email().toLowerCase()
+  
+  const filterName = { name: Name.toName(name) }
+  const filterEmail = { email: Email.toEmail(email) }
+
+  const objFiltered = generateObjectFilters<IValueObject>(filterName, filterEmail)
+
+  expect(objFiltered).toStrictEqual(
+    expect.objectContaining({
+      name,
+      email
+    })
+  )
+})
