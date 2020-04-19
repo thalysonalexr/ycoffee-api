3d1fe3aea3aa2ebcd2d8edef72200da0a9b6cdfd
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-04-19 13:15:23 -0400

    change types

--- backend/src/@types/process-env.d.ts
+++ backend/src/@types/process-env.d.ts
@@ -1,11 +1,11 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     NODE_ENV: 'development' | 'production' | 'test'
-    PORT: number
+    PORT: string
     BASE_URL: string
     API_NAME: string
     MONGO_URL: string
-    MONGO_PORT: number
+    MONGO_PORT: string
     MONGO_USER: string
     MONGO_PASS: string
     MONGO_DB: string
