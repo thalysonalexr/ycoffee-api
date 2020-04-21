577b5feaaf2f7a0cd9cea5136e4d718e917a2ac3
Thalyson Rodrigues <thalysonrodrigues.dev@gmail.com>
2020-04-21 00:10:18 -0400

    just little adjust

--- backend/src/app/middlewares/Validator.ts
+++ backend/src/app/middlewares/Validator.ts
@@ -1,15 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 import { validationResult } from 'express-validator'
 
-import { IMiddleware } from '@app/core/middlewares/IMiddleware'
+import { IMiddleware } from '@core/middlewares/IMiddleware'
 
 export class Validator implements IMiddleware {
   public process(req: Request, res: Response, next: NextFunction) {
     const errors = validationResult(req)
 
-    if (errors.isEmpty()) {
+    if (errors.isEmpty())
       return next()
-    }
 
     return res.status(400).json({
       error: 'Bad Request.',
